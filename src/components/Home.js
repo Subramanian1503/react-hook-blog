7e46f87ba66f5353c60d548ecfa0fac78ba286d2
Subramanian1503 <roopan1503@gmail.com>
2023-09-23 21:12:14 +0530

    Implement Home and Post detail component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,51 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import db from '../firebase';
+
 function Home() {
-  return <div className="">Home</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // Get all the post documents from firestore DB
+    db.collection('posts')
+      .get()
+      .then((docRefs) => {
+        // Convert the document into post with id
+        const posts = docRefs.docs.map((docRef) => {
+          return {
+            id: docRef.id,
+            ...docRef.data(),
+          };
+        });
+
+        // set the posts to the state of the component
+        setPosts(posts);
+      });
+  }, []);
+
+  return (
+    <div className="home">
+      {/* heading for tech blog */}
+      <h1>Tech Blog</h1>
+
+      {/* author name */}
+      <div id="blog-by">Subramanian</div>
+
+      {/* list of posts created  */}
+      {posts.map((post, index) => (
+        // creating post with the data
+        <div className="post" key={`post-${index}`}>
+          {/* heading of the post which links to the post detail */}
+          <Link to={`/post/${post.id}`}>
+            <h3>{post.title}</h3>
+          </Link>
+
+          {/* subtitle of the post */}
+          <p>{post.subTitle}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Home;
--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,5 +1,34 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import db from '../firebase';
+
 function PostDetail() {
-  return <div className="">PostDetail</div>;
+  const [post, setPost] = useState({});
+
+  // Get the post Id from the path param
+  const { postId } = useParams();
+
+  useEffect(() => {
+    // Get the post with postId from the firestore db
+    db.collection('posts')
+      .doc(postId)
+      .get()
+      .then((snapshot) => {
+        setPost(snapshot.data());
+      });
+
+    // Set state post with this data retrieved from the db
+  });
+
+  return (
+    <div className="post-detail">
+      {/* post title */}
+      <h1>{post.title}</h1>
+
+      {/* post content */}
+      <p>{post.content}</p>
+    </div>
+  );
 }
 
 export default PostDetail;
